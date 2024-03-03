7e35b63f37ef0b30c2554494b8d7f1f3c2cd4cbd
anmink94 <Anne.Minkenberg_EXT@wepa.eu>
2024-03-03 22:14:09 +0100

    post/ get db

--- Screens/Prediction.js
+++ Screens/Prediction.js
@@ -9,6 +9,7 @@ import {
   ScrollView
 } from 'react-native'
 import { useState, useEffect } from 'react'
+import axios from "axios"
 import UIButton from '../components/ui/UIButton'
 
 export default function Prediction({ route, navigation }) {
@@ -16,24 +17,44 @@ export default function Prediction({ route, navigation }) {
 
   const [selectedType, setSelectedType ] = useState();
   const [selectedColor, setSelectedColor ] = useState();
-  const [setSelectedSeason, selectedSeason ] = useState();
-  const [setSelectedReason, selectedReason ] = useState();
+  const [selectedSeason, setSelectedSeason ] = useState();
+  const [selectedReason, setSelectedReason ] = useState();
 
   useEffect(() => {
     setSelectedType(responseType)
     setSelectedColor(responseColor)
-    console.log('selectedColor', selectedColor)
-    console.log('selectedType', selectedType)
   }, [])
 
   const categoryType = ['Shirts', 'Bluse', 'Pullover', 'Jeans', 'Boots','Cardigan', 'Sneaker', 'High Heels', 'Hoodie', 'Rock', 'Hose', 'Kurze Hose', 'Langarm Shirt', 'Sweater', 'Top'];
-  const categoryColor = ['Blau', 'Braun', 'Gelb', 'Grau', 'Gruen', 'Lila', 'Magenta', 'Orange', 'Pink', 'Rot'];
+  const categoryColor = ['Blau', 'Braun', 'Gelb', 'Grau', 'Gruen', 'Lila', 'Magenta', 'Orange', 'Pink', 'Rot'];
   const categorySeason = ['Fruehling', 'Sommer', 'Herbst', 'Winter'];
   const categoryReason = ['Alle Styles', 'Classic', 'Casual', 'Streetwear', ' Party', 'Extravagant', 'Sport'];
 
   function handleButtonClickType(type) {
     setSelectedType(type)
-    console.log('handle', selectedType)
+  }
+
+  function handleButtonClickColor(color) {
+    setSelectedColor(color)
+  }
+
+  function handleButtonClickSeason(season) {
+    setSelectedSeason(season)
+  }
+
+  function handleButtonClickReason(reason) {
+    setSelectedReason(reason)
+  }
+
+  function onClickSave() {
+    axios.post('http://127.0.0.1:5000/put', {
+      type: selectedType,
+      color: selectedColor,
+      season: selectedSeason,
+      reason: selectedReason,
+      image: responseImage,
+    })
+    navigation.navigate('Test')
   }
 
   return (
@@ -56,25 +77,23 @@ export default function Prediction({ route, navigation }) {
         </View>
         <Text>Farbe des Kleidungsstück</Text>
         <View style={styles.buttonContainer}>
-          {/* {categoryColor.map((color) => (
-            <UIButton title={color} />
-          ))} */}
-          {categoryColor.map((color, index) => (
-            <Button key={index} title={color} color={responseColor === color ? 'red' : 'Black'} />
-          ))}
+          {categoryColor.map((color) => (
+            <UIButton title={color} onPress={() => handleButtonClickColor(color)}/>
+          ))} 
         </View>
         <Text>Jahreszeit</Text>
         <View style={styles.buttonContainer}>
           {categorySeason.map((season) => (
-            <UIButton title={season} />
+            <UIButton title={season} onPress={() => handleButtonClickSeason(season)}/>
           ))}
         </View>
         <Text>Kleidungsstil</Text>
         <View style={styles.buttonContainer}>
           {categoryReason.map((reason) => (
-            <UIButton title={reason} />
+            <UIButton title={reason} onPress={() => handleButtonClickReason(reason)}/>
           ))}
         </View>
+        <UIButton title={'Speichern'} onPress={() => onClickSave()} />
       </ScrollView>
     </SafeAreaView>
   )
--- Screens/Test.js
+++ Screens/Test.js
@@ -1,62 +1,45 @@
-import React, { useState, useEffect } from 'react'
-import { View, Image, Button } from 'react-native'
-import * as ImagePicker from 'expo-image-picker'
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
-  const [imageUri, setImageUri] = useState(null)
+export default function Test({route}) {
+  const [ data, setData] = useState(null); // Setze data auf null als Standardwert
 
   useEffect(() => {
-    ;(async () => {
-      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync()
-      if (status !== 'granted') {
-        alert('Permission to access media library is required!')
-      }
-    })()
-  }, [])
+    axios
+    .get("http://127.0.0.1:5000/")
+    .then(function (response) {
+      console.log('console', response.data);
+      setData(response.data)
+    });
+  }, []);
 
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    })
-
-    if (!result.cancelled) {
-      setImageUri(result.uri)
-    }
+  // Überprüfe, ob data definiert ist, bevor du darauf zugreifst
+  if (!data) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Lade...</Text>
+      </View>
+    );
   }
 
-  const removeBackground = async () => {
-    try {
-      const response = await fetch(imageUri)
-      const imageBlob = await response.blob()
-      const imageData = await imageBlob.arrayBuffer()
-
-      const removedBackgroundData = rembg.remove(Buffer.from(imageData))
-      const removedBackgroundUri = `data:image/png;base64,${removedBackgroundData.toString(
-        'base64'
-      )}`
-
-      // Verwende removedBackgroundUri für die Darstellung oder Speicherung des bearbeiteten Bildes
-      console.log('Hintergrund entfernt:', removedBackgroundUri)
-    } catch (error) {
-      console.error('Fehler beim Entfernen des Hintergrunds:', error)
-    }
-  }
-
+  // Wenn data definiert ist, kannst du darauf zugreifen
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      {imageUri && (
-        <Image
-          source={{ uri: imageUri }}
-          style={{ width: 200, height: 200, marginBottom: 10 }}
+      <Text>Kleiderschrank</Text>
+      <Text>{data.data[0].color}</Text>
+      <Text>{data.data[0].type}</Text>
+      <Text>{data.data[0].season}</Text>
+      <Text>{data.data[0].reason}</Text>
+      <Image
+          style={{
+            width: 300,
+            height: 500,
+            borderWidth: 1,
+            borderColor: 'red',
+          }}
+          source={{ uri: `data:image/png;base64,${data.data[0].image}` }}
         />
-      )}
-      <Button title="Bild auswählen" onPress={pickImage} />
-      {imageUri && (
-        <Button title="Hintergrund entfernen" onPress={removeBackground} />
-      )}
     </View>
-  )
+  );
 }
